e1a0cbd7bc7c57740af219c39dd9647d83882c81
somagh <aryakowsary@yahoo.com>
2017-02-24 15:53:33 +0330

    base

--- src/client/AI.java
+++ src/client/AI.java
@@ -18,7 +18,7 @@ import java.util.Random;
  */
 public class AI
 {
-
+    int[][][][] strategy=new int[2][3][2][3];
     public void doTurn(World game)
     {
         // fill this method, we've presented a stupid AI for example!
@@ -40,24 +40,44 @@ public class AI
                 game.changeType(beetle,BeetleType.HIGH);
         }
 
-        if (game.getCurrentTurn() == 0)
-        {
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 2; j++)
-                {
-                    for (int k = 0; k < 3; k++)
+        int[][][][][] scores=new int[2][3][2][3][3];
+        for(int i=0;i<2;i++)
+            for(int j=0;j<3;j++)
+                for(int k=0;k<2;k++)
+                    for(int l=0;l<3;l++)
                     {
-                        game.changeStrategy(BeetleType.LOW, CellState.values()[i], CellState.values()[j], CellState.values()[k], Move.values()[1]);
-                        game.changeStrategy(BeetleType.HIGH, CellState.values()[i], CellState.values()[j], CellState.values()[k], Move.values()[1]);
+                        scores[i][j][k][l][1] = 10; //moving forward is good
+                        scores[i][j][k][l][0] = 0;
+                        scores[i][j][k][l][2] = 0;
+                        scores[i][j][k][l][strategy[i][j][k][l]]+=20; //don't changing strategy is good
+                    }
+
+        for(Beetle bettle:beetles)
+        {
+            //set score for each move here
+        }
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 2; j++)
+                for (int k = 0; k < 3; k++) {
+                    int a = 1;
+                    if (scores[0][i][j][k][0] > scores[0][i][j][k][1] && scores[0][i][j][k][0] > scores[0][i][j][k][2])
+                        a = 0;
+                    if (scores[0][i][j][k][2] > scores[0][i][j][k][1] && scores[0][i][j][k][2] > scores[0][i][j][k][0])
+                        a = 2;
+                    if (a != strategy[0][i][j][k]) {
+                        game.changeStrategy(BeetleType.LOW, CellState.values()[i], CellState.values()[j], CellState.values()[k], Move.values()[a]);
+                        strategy[0][i][j][k] = a;
+                    }
+                    a = 1;
+                    if (scores[1][i][j][k][0] > scores[1][i][j][k][1] && scores[1][i][j][k][0] > scores[1][i][j][k][2])
+                        a = 0;
+                    if (scores[1][i][j][k][2] > scores[1][i][j][k][1] && scores[1][i][j][k][2] > scores[1][i][j][k][0])
+                        a = 2;
+                    if (a != strategy[1][i][j][k]) {
+                        game.changeStrategy(BeetleType.HIGH, CellState.values()[i], CellState.values()[j], CellState.values()[k], Move.values()[a]);
+                        strategy[1][i][j][k] = a;
                     }
                 }
-            }
-        } else
-        {
-            // no strategy change
-        }
-
     }
 
 }
